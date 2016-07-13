f0e937f1e276cdbfbe54395708d72e6d34a1429b
Aaron Lin <lin.aaron1@gmail.com>
2016-07-13 17:18:03 -0400

    empty char is added at the end of smiley instead of in between

--- demote.js
+++ demote.js
@@ -1,15 +1,16 @@
 var CHAT_SELECTOR = "_5rpu" 	// "._5rpu", "._1mf"
-var EMPTY_CHAR = "⁠"				// <-- THERE'S AN EMPTY CHARACTER IN THIS STRING
+var EMPTY_CHAR = "x" //"⁠"				// <-- THERE'S AN EMPTY CHARACTER IN THIS STRING
 var SMILEY_LIST = [":)", ":("]
 
 document.getElementById("pagelet_dock").addEventListener("input", function() {
 	if(event.target.classList.contains(CHAT_SELECTOR)) {
 		activeChat = event.srcElement;
 		activeSpan = getInnerSpan(activeChat);
+		activeSpan.style.backgroundColor = "#AAA#";
 		innerText = activeSpan.textContent;
+		console.log(activeSpan);
 		if (existsSmiley(innerText)) {
-			newText = insertEmptyChar(innerText);
-			replaceChatText(activeSpan, newText);
+			typeEmptyChar(activeSpan);
 		}
 	}
 });
@@ -41,8 +42,18 @@ function insertEmptyChar(innerText) {
 
 function replaceChatText(element, text) { 
 	// Taken from [1]
+	text = "<span> </span>";
+
     var textEvent = document.createEvent('TextEvent'); 
     textEvent.initTextEvent('textInput', true, true, window, text);
+    element.dispatchEvent(textEvent); 
+}
+// TODO: Race conditions?
+// Event constructors? [2]
+
+function typeEmptyChar(element) {
+    var textEvent = document.createEvent('TextEvent'); 
+    textEvent.initTextEvent('textInput', true, true, window, EMPTY_CHAR);;
     element.dispatchEvent(textEvent); 
 }
 
@@ -50,13 +61,21 @@ function replaceChatText(element, text) {
 
 
 
-
+// References
+// ------------
 // [1] http://stackoverflow.com/questions/35368880/set-or-replace-textcontent-in-facebooks-chat
+// [2] http://stackoverflow.com/questions/15951468/what-is-a-good-example-of-using-event-constructors-in-js
 
 /*
-
 QUESTIONS
 
 What do you do if the input chat is something like "text [[]] text"?
+Rapid key presses?
+Smileys in the middle of sentences
+This extension heats up your computer a lot?
+
+Make a separate span with the smiley?
 
 */
+
+
